988ea1b494f13293f307c36b4b2a4781cacd496f
Jackty89 <90223197+Jackty89@users.noreply.github.com>
2022-02-20 17:00:41 +0100

    cleanup

--- InstantTextDisplay.lua
+++ InstantTextDisplay.lua
@@ -1,35 +1,35 @@
-GameVersion = "3_80"
+GameVersion = "3_81"
 ModeName = "InstantTextDisplay"
 Author = "Jackty89"
 
 TextDelay = "0.0001"
 
-NMS_MOD_DEFINITION_CONTAINER = 
+NMS_MOD_DEFINITION_CONTAINER =
 {
     ["MOD_FILENAME"] 			= ModeName..GameVersion..".pak",
-    ["MOD_DESCRIPTION"]			= ModeName,   
+    ["MOD_DESCRIPTION"]			= ModeName,
     ["MOD_AUTHOR"]				= Author,
     ["NMS_VERSION"]				= GameVersion,
-    ["MODIFICATIONS"] 			= 
+    ["MODIFICATIONS"] 			=
 	{
 		{
-			["MBIN_CHANGE_TABLE"] 	= 
+			["MBIN_CHANGE_TABLE"] 	=
 			{
-                { 
-					["MBIN_FILE_SOURCE"] 	= "METADATA\UI\SPECIALTEXTPUNCTUATIONDELAYDATA.MBIN",
-					["EXML_CHANGE_TABLE"] 	= 
+                {
+					["MBIN_FILE_SOURCE"] 	= "METADATA\\UI\\SPECIALTEXTPUNCTUATIONDELAYDATA.MBIN",
+					["EXML_CHANGE_TABLE"] 	=
 					{
                         {
-                            ["MATH_OPERATION"] 		= "*", 				
+                            ["MATH_OPERATION"] 		= "*",
                             ["REPLACE_TYPE"] 		= "ALL",
-							["VALUE_CHANGE_TABLE"] 	= 
+							["VALUE_CHANGE_TABLE"] 	=
 							{
 								{"Delay", TextDelay},
                                 {"DefaultDelay", TextDelay}
 							}
 						}
                     }
-				}				
+				}
             }
         }
     }
